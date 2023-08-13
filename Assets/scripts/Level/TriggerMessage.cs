97ae79660c9fbe3fee1befb70bb83fae17c8ae18
billgatesperosubnormal <mateoconde30@gmail.com>
2023-08-13 22:35:10 +0200

    Sistema de mensajorado e implementado a full

--- Assets/scripts/Level/TriggerMessage.cs
+++ Assets/scripts/Level/TriggerMessage.cs
@@ -4,26 +4,57 @@ using UnityEngine;
 
 public class TriggerMessage : MonoBehaviour
 {
-    [Header("The name must match with the name in DisplayText")]
+    [Header("Type message here")]
+    [TextArea]
     public string whatMessage;
+
     [Header("Select what key you have to press to hide the message")]
     public KeyCode whatKey;
+
+    [Header("Activate timer if you don't want to use keycodes")]
+    public bool useTimer;
+    public float timer;
+    float timerStartValue;
     public DisplayText texto;
+
+    bool isEnabled;
+
     void Start()
     {
-        
+        timerStartValue = timer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(whatKey))
+        //This is to make the text dissapear if you press a key
+        if(Input.GetKeyDown(whatKey) && useTimer == false && isEnabled == true)
         {
             texto.HideText();
+            isEnabled = false;
+        }
+
+        //This makes it dissapear with a timer
+        if (useTimer && isEnabled)
+        {
+            if(timer >= 0)
+            {
+                timer -= Time.deltaTime;
+            }
+           else
+            {
+                texto.HideText();
+                isEnabled = false;
+            }
         }
     }
     private void OnTriggerEnter(Collider other)
     {
-        texto.ShowText(whatMessage);
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            texto.ShowText(whatMessage);
+            isEnabled = true;
+            timer = timerStartValue;
+        }
     }
 }
