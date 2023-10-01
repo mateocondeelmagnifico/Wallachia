69513a7f2e8ffa11fe8ebf795132e80eac84bf9b
MateoperoMateo <101583268+mateocondeelmagnifico@users.noreply.github.com>
2023-10-01 13:44:47 +0200

    Raycast para la cruz añadido

--- Assets/scripts/Weapons/Cross.cs
+++ Assets/scripts/Weapons/Cross.cs
@@ -11,6 +11,7 @@ public class Cross : MonoBehaviour
 
     public Light myLight;
 
+    public Transform crossPosition;
 
     private void Update()
     {
@@ -34,9 +35,27 @@ public class Cross : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.tag.Equals("Enemy"))
+        if (other.gameObject.tag.Equals("Enemy") && isActive)
         {
-            //Acess Enemy script
+            
+            #region Define Raycast Values
+            Vector3 enemyPosition = new Vector3(other.transform.position.x, other.transform.position.y + 1, other.transform.position.z);
+            Vector3 direction = enemyPosition - crossPosition.position;
+            Ray rayo = new Ray(crossPosition.position, direction);
+            RaycastHit hit;
+            #endregion
+
+            Debug.DrawRay(crossPosition.position, direction);
+            
+
+            if(Physics.Raycast(rayo, out hit, 2000, 1<< 8 | 1 << 11 | 1<< 0))
+            {
+                if (hit.collider.gameObject.tag.Equals("Enemy"))
+                {
+                    //Acess Enemy script
+                    Debug.Log("Enemy hit");
+                }
+            }
         }
     }
 }
